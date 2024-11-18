3e48a4aae387407736ac16502fb26613cb71a18e
Josh <josh@email.com>
2024-11-18 13:55:13 -0600

    init

---
+++ tokenizer/tokenizer.py
@@ -0,0 +1,100 @@
+import os
+import json
+import numpy as np
+
+spec_tokens = ['UNK', '<|startofgame|>', '<|endofgame|>']
+input_file_dir = "./data/clean/"
+
+class ChessTokenizer:
+    def __init__(self,
+             spec_tokens: list = ['UNK', '<|startofgame|>', '<|endofgame|>'],
+             input_file_dir: str = "./data/clean/",
+             output_file: str = "./tokenizer/vocab.json"):
+        self.spec_tokens = spec_tokens
+        self.input_file_dir = input_file_dir
+        self.output_file = output_file
+        self.tokens = dict()
+
+    def train(self):
+        """
+        Creates the vocab for the tokenizer
+        Vocab will be in format {str:int}
+        """
+        for tok in spec_tokens:
+            self.tokens[tok] = len(self.tokens)
+
+        for (_, _, files) in os.walk(input_file_dir):
+            for file in files:
+                with open(input_file_dir + file, 'r') as game_file:
+                    for line in game_file:
+                        new_tok = line.strip()
+                        if new_tok not in self.tokens:
+                            self.tokens[new_tok] = len(self.tokens)
+
+                game_file.close()
+
+    def save_tokenizer(self, outfile=None):
+        """
+        Saves vocab to outfile
+        """
+        outfile = self.output_file if outfile is None else outfile
+
+        with open(outfile, "w") as json_file:
+            print(f"Writing tokens to {outfile}")
+            json.dump(self.tokens, json_file, indent=4)
+
+
+    def load_tokenizer(self, filepath=None):
+        """
+        Loads a tokenizer into self.tokens
+        """
+        try:
+            with open(filepath, "r") as f:
+                self.tokens = json.load(f)
+        except:
+            print(f"[-] Could not open vocab file \"{filepath}\"")
+
+    def invert_tokens(self):
+        if not isinstance(self.tokens, dict):
+            raise ValueError("self.tokens must be a dictionary")
+
+        # Invert the dictionary
+        inverted_tokens = {value: key for key, value in self.tokens.items()}
+
+        return inverted_tokens
+
+    def encode(self, moves: list) -> np.ndarray:
+        """
+        Encode sequence of moves to tokens
+        """
+        seq = np.empty_like(moves)
+
+        for i, move in enumerate(moves):
+            seq[i] = self.tokens[move]
+
+        return seq
+
+    def decode(self, tokens: np.ndarray) -> list:
+        """
+        Decode tokens to array of moves
+        """
+        seq = list()
+        itokens = self.invert_tokens()
+
+        for tok in tokens:
+            seq.append(itokens[int(tok)])
+
+        return np.array(seq)
+
+if __name__ == "__main__":
+    test_game = ["<|startofgame|>", "e2e4", "c7c5", "g1f3", "d7d6", "f1b5", "c8d7", "d1e2", "g8f6", "b2b3", "e7e6", "c1b2", "f8e7", "e4e5", "d6e5", "f3e5", "e8g8", "e1g1", "a7a6", "e5d7", "b8d7", "b5d3", "b7b5", "a2a4", "c5c4", "b3c4", "b5b4", "d3e4", "a8b8", "d2d3", "a6a5", "b1d2", "d7c5", "b2e5", "b8b6", "e4f3", "d8d7", "d2b3", "b6a6", "b3c5", "e7c5", "d3d4", "c5e7", "f1d1", "f8c8", "c4c5", "a6a7", "e2b5", "f6d5", "b5d7", "a7d7", "d1d3", "f7f6", "f3g4", "g8f7", "e5g3", "f6f5", "g4h5", "g7g6", "h5f3", "e7f6", "g3d6", "d7d6", "c5d6", "c8c2", "f3d5", "e6d5", "a1e1", "c2c6", "d6d7", "c6d6", "e1e8", "d6d7", "e8a8", "f6d8", "g2g3", "f7e6", "a8a6", "e6f7", "g1g2", "g6g5", "g2f3", "d8c7", "a6a7", "g5g4", "f3g2", "f7e6", "a7b7", "e6d6", "b7b5", "d7e7", "g2f1", "e7e4", "b5b7", "h7h5", "b7b5", "f5f4", "f2f3", "g4f3", "g3f4", "e4f4", "f1f2", "c7d8", "b5b7", "d8h4", "f2f1", "f3f2", "b7b6", "1-0", "<|endofgame|>"]
+
+
+    tokenizer = ChessTokenizer()
+    # tokenizer.train()
+    # tokenizer.save_tokenizer()
+
+    tokenizer.load_tokenizer("./tokenizer/vocab.json")
+    seq = tokenizer.encode(test_game)
+    seq = tokenizer.decode(seq)
+    print((seq == test_game).all())
---
+++ utils/extract_pgn.py
@@ -0,0 +1,50 @@
+import chess.pgn
+import sys
+import os
+
+# Define the path to the input PGN file
+input_file_dir = "./data/games/"
+output_file_dir = "./data/clean/"
+output_data = []
+
+for (_, _, files) in os.walk(input_file_dir):
+    for file in files:
+        if file.endswith(".pgn"):
+            input_file = input_file_dir + file
+            output_file = output_file_dir + file[:-4] + ".txt"
+            with open(input_file, "r", encoding="utf-8") as pgn_file:
+                while True:
+                    game = chess.pgn.read_game(pgn_file)
+                    if game is None:
+                        break
+
+                    # Extract the headers
+                    white_elo = game.headers.get("WhiteElo")
+                    black_elo = game.headers.get("BlackElo")
+                    result = game.headers.get("Result")
+
+                    try:
+                        white_elo = int(white_elo) if white_elo and white_elo.isdigit() else None
+                        black_elo = int(black_elo) if black_elo and black_elo.isdigit() else None
+                    except ValueError:
+                        continue
+
+                    # Check if either player has an Elo rating over 2200
+                    if (white_elo and white_elo > 2200) or (black_elo and black_elo > 2200):
+                        # Extract the move sequence
+                        moves = game.mainline_moves()
+
+                        o = ['<|startofgame|>']
+                        for m in moves:
+                            o.append(str(m))
+                        o.append(result)
+                        o.append('<|endofgame|>')
+
+                        output_data.append(o)
+            with open(output_file, 'w') as f:
+                print(f"Writing data to {output_file}")
+                for game in output_data:
+                    for move in game:
+                        f.write(move)
+                        f.write("\n")
+                f.close()
